45c66f97ece4cb2b4797b0f598bee013c5a556a2
Daphnetta <svet001@bk.ru>
2023-05-16 16:11:51 +0300

    update

--- Accretion_disk.py
+++ Accretion_disk.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 from math import log, log10, sqrt, sin, cos, asin, radians, degrees, pi, exp
-import cmath
 from scipy import integrate
 import numpy as np
 
@@ -13,7 +12,7 @@ M_dot = M_star*3.1536e-1  # g/c
 R_D = 1.5e15              # cm
 m_dot = 1
 m = 1
-i = 1                     # grad
+i = radians(0)                     # grad
 T_eff_star = 4e3          # K
 
 G = 6.67e-8               # cm3*g-1*s-2
@@ -22,44 +21,44 @@ h = 6.626e-27             # g*cm2*s-1
 c = 3e10                  # cm/s
 k_B = 1.38e-16            # g*cm2*s-2*K-1
 
-lambda_min = 1e-7         # cm
+lambda_min = 1e-12         # cm
 lambda_max = 1e-1         # cm
 N = 200
 log_lambdas = np.linspace(log10(lambda_min), log10(lambda_max), N)
 lambdas = 10**log_lambdas
 
-r_min = R_star  # cm
-r_max = R_D # cm
-Nr = 200
-log_rs = np.linspace(log10(r_min), log10(r_max), Nr)
-rs = 10**log_rs
+x_min = 1
+x_max = R_D / R_star
 
 lamst = [] 	              # пустой список для значений lambda
 lamfst = []               # пустой список для значений lambda*F_lambda
 
-def T_D_eff(r):
-	return 150*(r/(1.5*10**13))**-0.75
+def T_D_eff(x):
+	return 150*(x/(1.5*10**13))**-0.75
+
+def gamma_0(x, i):
+    if ((1 < x) and (x < 1.0 / cos(i))):
+        return asin(sqrt(1 - x**(-2)) / sin(i))
+    else:
+        return 0.5 * pi
+
+def subint(x, lam):
+    arg = h*c / (lam*k_B*T_D_eff(x*R_star))
+    if (arg < 700):
+        return x*(pi + 2.0 * gamma_0(x, i)) / (exp(arg) - 1)
+    else:
+        return x*(pi + 2.0 * gamma_0(x, i)) / (arg)
 
 for lam in lambdas:
 	lamst.append(lam)
 	print(lam)
-	b = ((2*h*c**2)/(lam**5))*((R_star/d)**2)* cos(radians(i)) # константа к I_lambda
+	b = ((2*h*c**2)/(lam**5))*((R_star/d)**2)* cos(i) # константа к I_lambda
 	f_all_1 = (0,0)
-	f_all_2 = (0,0)
-	for r in rs :
-		x = r/R_star
-		if x>1 and x<(1/cos(radians(i))):
-			gamma_0 = lambda x: asin((cmath.sqrt(1-(x)**(-2)))/sin(radians(i)))
-			Interg = lambda x: b*((pi+2*gamma_0(x))/(exp((h*c)/(lam*k_B*T_D_eff(x)))-1))*x
-			r_1 = integrate.quad(Interg, 3, R_D/R_star)
-			f_all_1 = (f_all_1[0]+r_1[0], sqrt(f_all_1[1]**2+r_1[1]**2))	
-		elif x>(1/cos(radians(i))) :
-			gamma_0 = pi/2
-			Interg = lambda x: b*((pi+2*gamma_0)/(exp((h*c)/(lam*k_B*T_D_eff(x)))-1))*x
-			r_2 = integrate.quad(Interg, 3, R_D/R_star)
-			f_all_2 = (f_all_2[0]+r_2[0], sqrt(f_all_2[1]**2+r_2[1]**2))
-		#print(rs)
-	lamfst.append(lam*(f_all_1[0]+f_all_2[0])) 
+	Interg = lambda x: b*subint(x, lam)
+	r_1 = integrate.quad(Interg, x_min, x_max)
+	f_all_1 = (f_all_1[0]+r_1[0], sqrt(f_all_1[1]**2+r_1[1]**2))	
+	#print(rs)
+	lamfst.append(lam*(f_all_1[0])) 
 
 ax = plt.gca()
 ax.set_xscale("log")
